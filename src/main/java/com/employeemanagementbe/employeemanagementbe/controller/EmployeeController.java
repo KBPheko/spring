95b1b4644411d79685141be0f1e923b3a2221d87
KBPheko <@J8riW2aG*>
2023-05-03 13:11:34 +0200

    changes made

--- src/main/java/com/employeemanagementbe/employeemanagementbe/controller/EmployeeController.java
+++ src/main/java/com/employeemanagementbe/employeemanagementbe/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.employeemanagementbe.employeemanagementbe.models.Employee;
 import com.employeemanagementbe.employeemanagementbe.repo.EmployeeRepo;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/employee") //this is a url request
 public class EmployeeController {
     
